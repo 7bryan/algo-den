1b974b38e4bc5f660df8264841bdf2b19602048d
7bryan <bryanaw.bjm@gmail.com>
2026-08-28 23:59:40 +0800

    practicing more dp

---
+++ practice/dp/vitamin.cpp
@@ -0,0 +1,37 @@
+// KSN P 2021 - bagian B > B3
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> harga;
+vector<int> kandungan;
+vector<int> dosis;
+
+vector<int> dp(m + 1, 0);
+
+int solve() {
+  for (int i = 0; i < n; i++) {
+    if (dosis[i] == 1) {
+      // 0/1 knapsack
+    } else {
+      // unbound knapsack
+    }
+  }
+}
+
+int main() {
+  // Optimize I/O operations
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cin >> n >> m;
+  for (int i = 0; i < n; i++) {
+    int h, k, d;
+    cin >> h >> k >> d;
+    harga.push_back(h);
+    kandungan.push_back(k);
+    dosis.push_back(d);
+  }
+
+  return 0;
+}
