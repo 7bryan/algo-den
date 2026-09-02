889773c78a74baaa47fe1d1209360061e6bfba98
7bryan <bryanaw.bjm@gmail.com>
2026-09-02 23:35:17 +0800

    idk what I'm doing

---
+++ practice/searching/televisi.cpp
@@ -0,0 +1,16 @@
+// OSN 2023 > 2A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+  // Optimize I/O operations
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ll n, p;
+  cin >> n >> p;
+
+  return 0;
+}
