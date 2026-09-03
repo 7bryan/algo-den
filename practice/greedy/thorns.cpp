7de6b40d7cba2b0867bad3b03c743ce1cd28b424
7bryan <bryanaw.bjm@gmail.com>
2026-09-03 21:13:32 +0800

    another greedy problem

---
+++ practice/greedy/thorns.cpp
@@ -0,0 +1,36 @@
+// codeforces 1932A
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve() {
+  int len;
+  cin >> len;
+  string cells;
+  cin >> cells;
+
+  int coins = 0;
+  for (int i = 0; i < len; i++) {
+    char cell = cells[i];
+
+    if (cell == '@')
+      coins++;
+    else if (cell == '*' && cells[i + 1] == '*' && i < len - 2)
+      return coins;
+  }
+
+  return coins;
+}
+
+int main() {
+  // Optimize I/O operations
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    cout << solve() << "\n";
+  }
+
+  return 0;
+}
