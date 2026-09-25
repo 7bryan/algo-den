39ae0b7797d373a985247abdd0608d2f87ef7d23
7bryan <bryanaw.bjm@gmail.com>
2026-09-26 00:43:21 +0800

    update

---
+++ practice/implementation/garis.cpp
@@ -0,0 +1,11 @@
+// TROC #42 > C
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // Optimize I/O operations
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  return 0;
+}
---
+++ practice/searching/batavia.cpp
@@ -0,0 +1,11 @@
+// OSN informatika 2024 > 1B
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // Optimize I/O operations
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  return 0;
+}
