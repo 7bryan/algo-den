e7dcf9884f9d323cd801e4bdc467bdfe1efdc882
7bryan <bryanaw.bjm@gmail.com>
2026-09-26 00:38:49 +0800

    update

---
+++ practice/dp/mooney.cpp
@@ -0,0 +1,15 @@
+// usaco time is mooney
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // Optimize I/O operations
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m, c;
+  cin >> n >> m >> c;
+  while ()
+
+    return 0;
+}
